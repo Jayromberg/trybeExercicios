4ecacf28682ffb9cc122c98373589d168b67ea08
Jayromberg <jayro.dev@gmail.com>
2022-03-08 19:37:40 -0300

    4.2 Exercicio 1 e 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/arrayELoopFor.js
@@ -0,0 +1,19 @@
+//Execício 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index++){
+    console.log(numbers[index]);
+}
+
+//Exercício 2
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let soma = 0;
+for (let index = 0; index < numbers.length; index++){
+    soma = soma + numbers[index];
+}
+console.log(soma);
+
+//Exercício 3
