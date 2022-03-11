60a78c99804333f7965dc9b905ed1b3886e8a65a
Jayromberg <jayro.dev@gmail.com>
2022-03-11 14:03:30 -0300

    4.4 exercicio concluido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objectsAndForIn.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/objectsAndForIn.js
@@ -44,14 +44,15 @@ for (let key in info) {
     if (info[key] !== info2[key]) {
         console.log(info[key] + " e " + info2[key]);
     }
-    else{
+    else {
         console.log("Ambos", key + "s");
     }
 }
 console.log('');
 
 
-// 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+// 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log 
+// no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
 
 let leitor = {
     nome: 'Julia',
@@ -59,19 +60,38 @@ let leitor = {
     idade: 21,
     livrosFavoritos: [
         {
-            titulo: 'O Pior Dia de Todos',
+            titulo: '"O Pior Dia de Todos"',
             autor: 'Daniela Kopsch',
             editora: 'Tordesilhas',
         },
     ],
 };
 
-// 7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
+console.log('O livro favorito de', leitor.nome, leitor.sobrenome, 'se chama', leitor.livrosFavoritos[0]["titulo"] + '.');
+console.log('');
 
-{
+// 7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
+// {
+//     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//     autor: 'JK Rowling',
+//     editora: 'Rocco',
+// }
+
+leitor.livrosFavoritos[1] = {
     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-        autor: 'JK Rowling',
-            editora: 'Rocco',
-}
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+};
+
+console.log(leitor.livrosFavoritos);
+console.log('');
 
 // 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+
+if(leitor.livrosFavoritos.length - 1 === 0){
+    console.log(leitor.nome,'tem 1 livro favorito');
+}
+else{
+    console.log(leitor.nome,'tem',leitor.livrosFavoritos.length,'livros favoritos.');
+}
+console.log('');
