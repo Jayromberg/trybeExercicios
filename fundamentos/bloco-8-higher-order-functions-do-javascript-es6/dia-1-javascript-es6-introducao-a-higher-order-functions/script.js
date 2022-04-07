6aa0cb4650ab4d47d0299211771699d4b7c918bc
Jayromberg <jayro.dev@gmail.com>
2022-04-07 12:12:19 -0300

    Para fixar

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -0,0 +1,13 @@
+// Para fixar
+
+const wakeUp = () => 'Acordou!!';
+const breakfast = () => 'Bora tomar café!!';
+const toSleep = () => 'Partiu dormir!!';
+
+const doingThings = (func) => console.log(func());
+
+doingThings(wakeUp);
+doingThings(breakfast);
+doingThings(toSleep);
+
+// Agora a prática
