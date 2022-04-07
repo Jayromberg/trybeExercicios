2f96f69232a9fc4735096b1de22b4fd6f4adc175
Jayromberg <jayro.dev@gmail.com>
2022-04-07 12:46:30 -0300

    Exercicio 1

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -11,3 +11,24 @@ doingThings(breakfast);
 doingThings(toSleep);
 
 // Agora a prática
+
+// Exercício 1
+
+const users = (name, lastName) => {
+    const object = {
+        nomeCompleto: `${name} ${lastName}`,
+        email: `${name}_${lastName}@trybe.com`.toLowerCase(),
+    };
+    return object;
+}
+
+const newEmployees = () => {
+    const employees = {
+        id1: users('Pedro', 'Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+        id2: users('Luiza', 'Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+        id3: users('Carla', 'Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+};
+
+console.log(newEmployees());
