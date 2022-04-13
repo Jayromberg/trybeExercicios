8c4a397cb6878bd0ce8bb9e6c8ae3da049edb4f2
Jayromberg <jayro.dev@gmail.com>
2022-04-12 21:58:09 -0300

    Exercise 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -2,10 +2,10 @@ const arrays = [
     ['1', '2', '3'],
     [true],
     [4, 5, 6],
-  ];
-  
-  function flatten(array) {
-   return array.reduce((acc, array) => [`${acc}, ${array}`]);
-  }
+];
 
-  console.log(flatten(arrays))
+function flatten() {
+    return arrays.reduce((acc, curr) => acc.concat(curr), []);
+}
+
+console.log(flatten(arrays))
