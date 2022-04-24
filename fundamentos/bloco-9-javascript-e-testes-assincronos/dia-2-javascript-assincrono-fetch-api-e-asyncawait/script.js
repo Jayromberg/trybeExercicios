cd8ab7fea8afa89a3519197b26250ad4091c84c0
Jayromberg <jayro.dev@gmail.com>
2022-04-24 11:20:27 -0300

    Exercise 1

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script.js
@@ -1,8 +1,16 @@
-// apiScript.js
+// apiScript.js     
 const API_URL = 'https://icanhazdadjoke.com/';
+const jokeContainer = document.getElementById('jokeContainer');
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+
+  fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => jokeContainer.innerText = data.joke);
 };
 
 window.onload = () => fetchJoke();
