9ea1f86b52507cc7e115611d4a46c86a65c6e314
Jayromberg <jayro.dev@gmail.com>
2022-04-24 12:49:02 -0300

    Exercise 2 - concluded

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script2.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script2.js
@@ -2,14 +2,18 @@
 const API_URL = 'https://api.coincap.io/v2/assets';
 const coinContainer = document.getElementById('coinContainer');
 
-const fetchCoin = () => {
-    fetch(API_URL)
+const fetchCoin = async () => {
+   await fetch(API_URL)
         .then((response) => response.json())
-        .then((data) => data.data.forEach(element => {
-            const create = document.createElement('p');
-            create.innerHTML = `${element.name} (${element.symbol}): ${element.priceUsd}`;
-            coinContainer.appendChild(create);  
-        }));
+        .then((data) => data.data.forEach((element) => {
+            if (Number(element.rank) <= 10) {
+                const usdPrice = Number(element.priceUsd);
+                const create = document.createElement('p');
+                create.innerHTML = `${element.name} (${element.symbol}): ${usdPrice.toFixed(2)}`;
+                coinContainer.appendChild(create);
+            }
+        }))
+        .catch((error) => error.toString());
 };
 
 window.onload = () => fetchCoin();
